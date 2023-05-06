50e775bdb3f04b037bb709568c8409f2822ebaaa
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-05-06 20:49:51 -0300

    testing lenis window

--- src/components/LenisController/index.tsx
+++ src/components/LenisController/index.tsx
@@ -3,14 +3,14 @@ import Lenis from "@studio-freight/lenis"
 import { useLayoutEffect } from "react"
 
 export default function LenisController({ children }: { children: React.ReactNode }) {
-  useLayoutEffect(() => { 
-    const lenis = new Lenis()
+  const lenis: Lenis = new Lenis()
 
-    function raf(time: number) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-    
+  function raf(time: number) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  useLayoutEffect(() => { 
     requestAnimationFrame(raf) 
     //eslint-disable-next-line
   }, []) 

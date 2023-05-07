5782db5e4d3c2820c613595cd9c64a4f7b435c34
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-05-06 21:40:56 -0300

    a

--- src/components/LenisController/index.tsx
+++ src/components/LenisController/index.tsx
@@ -3,9 +3,9 @@ import Lenis from "@studio-freight/lenis"
 import { useLayoutEffect } from "react"
 
 export default function LenisController({ children }: { children: React.ReactNode }) {
-  const lenis: Lenis = new Lenis()
-
   function raf(time: number) {
+    const lenis: Lenis = new Lenis()
+
     lenis.raf(time)
     requestAnimationFrame(raf)
   }
